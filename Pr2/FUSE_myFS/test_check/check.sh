953610b45c03cf7bdd4ab12090ec5e7d0fec88fc
Álvaro García Barragán <79256834+Alvaro8gb@users.noreply.github.com>
2021-10-29 20:01:03 +0200

    up

--- Pr2/FUSE_myFS/test_check/check.sh
+++ Pr2/FUSE_myFS/test_check/check.sh
@@ -4,12 +4,28 @@ echo Executing check.sh .....
 
 cd ..
 
-temp_path='./mount-pount/temp/'
+MPOINT="./mount-point/"
+temp_path='./temp/'
 
 mkdir $temp_path
 
-cp fuseLib.c $temp_path
-cp myFS.h $temp_path
+file1='./src/fuseLib.c'
+file2='./src/myFS.h'
 
+cp $file1 $file2 $temp_path
+
+pwd
+ls $temp_path
+
+cp $file1 $file2 $MPOINT
+
+echo diff between copys : 
+
+diff $file1 $MPOINT'fuseLib.c'
+diff $file2 $MPOINT'myFS.h'
+
+echo End
+
+rm -r $temp_path
 
 
